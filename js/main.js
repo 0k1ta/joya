a7d82189612533dd9c617bf3189d9c18244fb67c
0k1ta <freeer55@gmail.com>
2020-10-21 22:19:57 +0900

    変数宣言のvarをconstに変更

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
 // ページが読み込まれたら実行
-window.onload = ()=>{
+window.onload = () => {
 
      // オブジェクトと変数の準備
-     var count_disp = document.getElementById("disp_count");  
-     var kane = document.getElementById("kane");
-     var reset_btn = document.getElementById("btn_reset");
-     var count_value = 108;
+     const count_disp = document.getElementById("disp_count");
+     const kane = document.getElementById("kane");
+     const reset_btn = document.getElementById("btn_reset");
+     const count_value = 108;
 
      // 除夜の鐘のクリック処理
-     kane.onclick = ()=>{
+     kane.onclick = () => {
           count_value -= 1;
           count_disp.innerHTML = count_value;
-          if(count_disp.innerHTML <= 0){
-            count_disp.innerHTML = "煩悩退散!"
+          if (count_disp.innerHTML <= 0) {
+               count_disp.innerHTML = "煩悩退散!"
           }
      };
      // 除夜の鐘のマウスダウン処理
-     kane.onmousedown = ()=>{
+     kane.onmousedown = () => {
           kane.style.backgroundColor = "#00FF00";
      }
      // 除夜の鐘のマウスアップ処理
-     kane.onmouseup = ()=>{
+     kane.onmouseup = () => {
           kane.style.backgroundColor = "";
      }
      // リセットボタンのクリック処理
-     reset_btn.onclick = ()=>{
+     reset_btn.onclick = () => {
           count_value = 108; count_disp.innerHTML = count_value;
      }
      // リセットボタンのマウスダウン処理
-     reset_btn.onmousedown = ()=>{
+     reset_btn.onmousedown = () => {
           reset_btn.style.backgroundColor = "#00FF00";
      }
      // リセットボタンのマウスアップ処理
-     reset_btn.onmouseup = ()=>{
+     reset_btn.onmouseup = () => {
           reset_btn.style.backgroundColor = "";
      }
 };
